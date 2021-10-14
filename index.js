b6b65fbca6080cdab4344f961cee20c927c71a1d
kartik mani <kartikmaniji@gmail.com>
2021-10-14 22:24:48 +0530

    done till question 5

--- index.js
+++ index.js
@@ -1,64 +1,94 @@
-  Q1. Write a function to get the sum of n natural numbers. 
+//   Q1. Write a function to get the sum of n natural numbers. 
 
-  function numberSum(N) {
-    var total = 0;
-      for(var i = 1; i <= N; i++){
-        total += i;
-      }
-      return total;
-  }
+//   function numberSum(N) {
+//     var total = 0;
+//       for(var i = 1; i <= N; i++){
+//         total += i;
+//       }
+//       return total;
+//   }
 
-  Q2. write the function to check if the number is even or not by using only if else statement 
-  
-  function oddOrEven(x) {
-    return ( x & 1 ) ? "odd" : "even";
-  }
-  
-  function checkNumber(argNumber) {
-    document.getElementById("result").innerHTML = "Number " + argNumber + " is " + oddOrEven(argNumber);
-  }
 
-  Q3. Write a function it check if the number is prime or not.
+//   Q2. write the function to check if the number is even or not by using only if else statement 
   
-  function isPrime(num) {
-    if (num === 2) {
-      return true;
-    } else if (num > 1) {
-      for (var i = 2; i < num; i++) {
+//   function oddOrEven(x) {
+//     return ( x & 1 ) ? "odd" : "even";
+//   }
   
-        if (num % i !== 0) {
-          return true;
-        } else if (num === i * i) {
-          return false
-        } else {
-          return false;
-        }
-      }
-    } else {
-      return false;
-    }
-  
-  }
+//   function checkNumber(argNumber) {
+//     document.getElementById("result").innerHTML = "Number " + argNumber + " is " + oddOrEven(argNumber);
+//   }
 
-  Q4. Write a function ‘weekDay’ that takes n as the input and returns the day associated to it. If n<1 or n>7 then return invalid input. Using switch case only
 
-  function weekDay(n){
-    let ans='';
-    switch(n){
-        case 1: ans='monday';
-            break;
-        case 2: ans='tuesday';
-            break;
-        case 3: ans='wednesday';
-            break;
-        case 4: ans='thursday';
-            break;
-        case 5: ans='friday';
-            break;
-        case 6: ans='satuday';
-            break;
-        case 7: ans='sunday';
-            break;
-            default : ans='invalid Input';
-    }                                                                     return ans;
+//   Q3. Write a function it check if the number is prime or not.
+  
+//   function isPrime(num) {
+//     if (num === 2) {
+//       return true;
+//     } else if (num > 1) {
+//       for (var i = 2; i < num; i++) {
+  
+//         if (num % i !== 0) {
+//           return true;
+//         } else if (num === i * i) {
+//           return false
+//         } else {
+//           return false;
+//         }
+//       }
+//     } else {
+//       return false;
+//     }
+  
+//   }
+
+//   Q4. Write a function ‘weekDay’ that takes n as the input and returns the day associated to it. If n<1 or n>7 then return invalid input. Using switch case only
+
+//   function weekDay(n){
+//     let ans='';
+//     switch(n){
+//         case 1: ans='monday';
+//             break;
+//         case 2: ans='tuesday';
+//             break;
+//         case 3: ans='wednesday';
+//             break;
+//         case 4: ans='thursday';
+//             break;
+//         case 5: ans='friday';
+//             break;
+//         case 6: ans='satuday';
+//             break;
+//         case 7: ans='sunday';
+//             break;
+//             default : ans='invalid Input';
+//     }                                                                     return ans;
+// }
+
+// //Q5. Write a function ‘weekDay’ that takes n as the input and returns the day associated to it. If n<1 or n>7 then return invalid input. Using If Else only.
+
+function weekDay(N){
+    if(N==1)
+        console.log('monday');
+
+    if(N==1)
+        console.log('tuesday');
+    
+    if(N==1)
+        console.log('wednesday');
+    
+    if(N==1)
+        console.log('thursday');
+    
+    if(N==1)
+        console.log('friday');
+    
+    if(N==1)
+        console.log('satuday');
+    
+    if(N==1)
+        console.log('sunday');
+    
+    else
+        console.log('invalid Input');
 }
